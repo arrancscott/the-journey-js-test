a85195b9ed278a2b15d633f21dd4cf8db50b9b8e
Arran <arran.scott@thefloow.com>
2015-01-22 23:50:46 +0000

    Ajax call, all working, just needs responsive side sorting out

--- sdf-generator.rb
+++ sdf-generator.rb
@@ -1,5 +1,27 @@
 require 'sinatra'
+require 'sinatra/contrib'
+require 'json'
 
 get '/' do
   erb :index
+end
+
+post '/generate/:size', provides: :json do
+	size = params[:size]
+	case size
+	when 'small'
+		s = 75
+	when 'medium'
+		s = 125
+	when 'large'
+		s = 175
+	else
+		s = 50
+	end
+	array = []
+	sdf = 'asdfas'.capitalize
+	s.to_i.send(:times) { array << sdf }
+	sdf = array.join.split('').shuffle.join
+	sdf = sdf.gsub(/(?<=[a-z])(?=[A-Z])/, ' ').downcase.capitalize + '.'
+	json sdf: sdf
 end
