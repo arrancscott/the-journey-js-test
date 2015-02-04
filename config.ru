119d65a4647c1854a175fb284c4c13ba87b9b775
Arran <arran.scott@thefloow.com>
2015-02-04 23:57:33 +0000

    Added class to name, added config.ru, gitignore and unicorn files

---
+++ config.ru
@@ -0,0 +1,6 @@
+require "rubygems"
+require "sinatra"
+
+require File.expand_path '../sdf-generator.rb', __FILE__
+
+run SdfGenerator
--- sdf-generator.rb
+++ sdf-generator.rb
@@ -2,26 +2,28 @@ require 'sinatra'
 require 'sinatra/contrib'
 require 'json'
 
-get '/' do
-  erb :index
-end
-
-post '/generate/:size', provides: :json do
-	size = params[:size]
-	case size
-	when 'small'
-		s = 75
-	when 'medium'
-		s = 125
-	when 'large'
-		s = 175
-	else
-		s = 50
+class SdfGenerator < Sinatra::Base
+	get '/' do
+	  erb :index
 	end
-	array = []
-	sdf = 'asdfas'.capitalize
-	s.to_i.send(:times) { array << sdf }
-	sdf = array.join.split('').shuffle.join
-	sdf = sdf.gsub(/(?<=[a-z])(?=[A-Z])/, ' ').downcase.capitalize + '.'
-	json sdf: sdf
+
+	post '/generate/:size', provides: :json do
+		size = params[:size]
+		case size
+		when 'small'
+			s = 75
+		when 'medium'
+			s = 125
+		when 'large'
+			s = 175
+		else
+			s = 50
+		end
+		array = []
+		sdf = 'asdfas'.capitalize
+		s.to_i.send(:times) { array << sdf }
+		sdf = array.join.split('').shuffle.join
+		sdf = sdf.gsub(/(?<=[a-z])(?=[A-Z])/, ' ').downcase.capitalize + '.'
+		json sdf: sdf
+	end
 end
